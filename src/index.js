e55ce541dc4a571630b606b0a8d9353ec2911534
Lihaihua <583268519@qq.com>
2025-05-26 13:25:31 +0800

    commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import '@ant-design/v5-patch-for-react-19';
+import './index.css';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
